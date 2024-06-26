2781318e494787e72480bb2b86aecdb3c6cb5688
Howard Huang <hao@hwrd.me>
2024-06-26 12:29:15 -0500

    Fix weather script

--- plugins/weather.sh
+++ plugins/weather.sh
@@ -1,26 +1,18 @@
 #!/usr/bin/env bash
 
 IP=$(curl -s https://ipinfo.io/ip)
-LOCATION_JSON=$(curl -s https://ipinfo.io/"$IP"/json)
-
-LOCATION="$(echo "$LOCATION_JSON" | jq '.city' | tr -d '"')"
-REGION="$(echo "$LOCATION_JSON" | jq '.region' | tr -d '"')"
-_COUNTRY="$(echo "$LOCATION_JSON" | jq '.country' | tr -d '"')"
-
-# Line below replaces spaces with +
-LOCATION_ESCAPED="${LOCATION// /+}+${REGION// /+}"
-WEATHER_JSON=$(curl -s "https://wttr.in/$LOCATION_ESCAPED?format=j1")
+LOCATION=$(curl -s https://ipinfo.io/"$IP"/json | jq '.loc' | tr -d '"')
+WEATHER_JSON=$(curl -s "https://wttr.in/$LOCATION?format=j1")
 
 # Fallback if empty
 if [ -z "$WEATHER_JSON" ]; then
-
-  sketchybar --set "$NAME" label="$LOCATION"
-  sketchybar --set "$NAME".moon icon=
-
+  sketchybar --set "$NAME" label="UNKNOWN LOCATION"
   return
 fi
 
+CITY="$(echo "$WEATHER_JSON" | jq '.nearest_area[0].areaName[].value' | tr -d '"')"
+REGION="$(echo "$WEATHER_JSON" | jq '.nearest_area[0].region[].value' | tr -d '"')"
 TEMPERATURE=$(echo "$WEATHER_JSON" | jq '.current_condition[0].temp_F' | tr -d '"')
 WEATHER_DESCRIPTION=$(echo "$WEATHER_JSON" | jq '.current_condition[0].weatherDesc[0].value' | tr -d '"' | sed 's/\(.\{25\}\).*/\1.../')
 
-sketchybar --set "$NAME" label="$LOCATION, $REGION  ${TEMPERATURE}°F $WEATHER_DESCRIPTION"
+sketchybar --set "$NAME" label="$CITY, $REGION  ${TEMPERATURE}°F $WEATHER_DESCRIPTION"

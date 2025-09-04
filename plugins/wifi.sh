462ae3e8b623b885d0be09c794b9131c46289d1e
Howard Huang <hao@hwrd.me>
2025-09-04 12:16:57 -0500

    Improve Wi-Fi script to detect Zscaler connection

--- plugins/wifi.sh
+++ plugins/wifi.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
-INFO="$(networksetup -listallhardwareports | awk '/Wi-Fi/{getline; print $2}' | xargs ipconfig getsummary | awk -F ' SSID : ' ' / SSID : / {print $2}')"
+# https://apple.stackexchange.com/a/475678
+INFO="$(networksetup -listpreferredwirelessnetworks en0 | sed -n '2 p' | tr -d '\t')"
+
+# https://brettterpstra.com/2024/01/29/checking-for-a-vpn-connection-from-the-command-line/
+# 0 (utun4 not found) means off, anything else means on
+ZSCALER=$(ifconfig -a | grep -c utun4)
+if [ "$ZSCALER" -ne 0 ]; then
+  INFO="$INFO (Zscaler)"
+fi
 
 if [ -z "${INFO}" ]; then
   ICON="󰖪"
